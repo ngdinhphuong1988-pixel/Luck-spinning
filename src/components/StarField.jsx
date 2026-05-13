d6112f460922dda1cabe9e48d10fd64b776d3fa7
ngdinhphuong1988-pixel <ng.dinh.phuong1988@gmail.com>
2026-05-13 19:52:01 +0700

    Add files via upload

---
+++ src/components/StarField.jsx
@@ -0,0 +1,17 @@
+import { STARS } from '../constants'
+
+export default function StarField() {
+  return (
+    <>
+      {STARS.map(s => (
+        <div key={s.id} style={{
+          position:'fixed', top:s.top, left:s.left,
+          width:s.size, height:s.size, borderRadius:'50%',
+          background:'#fff', opacity:.4,
+          animation:`twinkle ${s.dur}s ${s.delay}s ease-in-out infinite alternate`,
+          pointerEvents:'none',
+        }} />
+      ))}
+    </>
+  )
+}
